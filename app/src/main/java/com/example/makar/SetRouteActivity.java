83c6e157bd5a84acbe09d41fc70924a545758ca9
acycoco <pink10257@naver.com>
2023-11-05 22:13:50 +0900

    feat: 출발역, 도착역 검색 시 역정보를 로그에 띄우는 기능 추가

--- app/src/main/java/com/example/makar/SetRouteActivity.java
+++ app/src/main/java/com/example/makar/SetRouteActivity.java
@@ -1,19 +1,130 @@
 package com.example.makar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.makar.databinding.ActivitySetRouteBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class SetRouteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO 앱이 시작화면에 초기화하는 코드 -> 나중에 옮겨야됨 (확실히 필요한지는 모르겠음)
+//        FirebaseApp.initializeApp(this);
+
         ActivitySetRouteBinding binding = ActivitySetRouteBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        //역 엑셀 파일을 db에 올리는 코드 (db초기화 시에만 씀)
+//        DataConverter databaseConverter = new DataConverter(this);
+//        databaseConverter.readExcelFileAndSave();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        binding.startStationSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if (!newText.isEmpty()) {
+                    CollectionReference collectionRef = db.collection("stations"); // 컬렉션 이름에 맞게 변경하세요.
+
+                    //newText로 시작하는 모든 역 검색
+                    Query query = collectionRef.orderBy("stationName")
+                            .startAt(newText)
+                            .endAt(newText + "\uf8ff");
+
+                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                StringBuilder result = new StringBuilder();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Station station = document.toObject(Station.class);
+
+                                    //TODO 받은 station(검색결과)을 화면에 어떻게 띄울지
+                                    result.append("역 이름: ").append(station.getStationName()).append("\n");
+                                    result.append("역 코드: ").append(station.getStationCode()).append("\n");
+                                    result.append("노선 이름: ").append(station.getLineNum()).append("\n");
+                                    result.append("철도 운영 기관 코드: ").append(station.getRailOpr()).append("\n\n");
+                                }
+
+                                Log.d("MAKAR", "검색 결과:\n" + result.toString());
+                            } else {
+                                Log.d("MAKAR", "검색 중 오류 발생: ", task.getException());
+                            }
+                        }
+
+                    });
+                }
+                return true;
+
+            }
+        });
+
+
+        binding.endStationSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if (!newText.isEmpty()) {
+                    CollectionReference collectionRef = db.collection("stations"); // 컬렉션 이름에 맞게 변경하세요.
+
+                    //newText로 시작하는 모든 역 검색
+                    Query query = collectionRef.orderBy("stationName")
+                            .startAt(newText)
+                            .endAt(newText + "\uf8ff");
+
+                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                StringBuilder result = new StringBuilder();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Station station = document.toObject(Station.class);
+
+                                    result.append("역 이름: ").append(station.getStationName()).append("\n");
+                                    result.append("역 코드: ").append(station.getStationCode()).append("\n");
+                                    result.append("노선 이름: ").append(station.getLineNum()).append("\n");
+                                    result.append("철도 운영 기관 코드: ").append(station.getRailOpr()).append("\n\n");
+                                }
+
+                                Log.d("MAKAR", "검색 결과:\n" + result.toString());
+                            } else {
+                                Log.d("MAKAR", "검색 중 오류 발생: ", task.getException());
+                            }
+                        }
+
+                    });
+                }
+                return true;
+
+            }
+        });
 
 
         //경로 찾기 버튼 클릭 리스너
