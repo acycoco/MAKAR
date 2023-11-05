617d555dcd6a2b4a7dde28d82e2058612d1af2eb
daeun084 <030804jk@naver.com>
2023-11-05 20:24:02 +0900

    fix:dialog_set_alarm.xml custom

---
+++ app/src/main/java/com/example/makar/Dialog/SetAlarmDialog.java
@@ -0,0 +1,13 @@
+package com.example.makar.Dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class SetAlarmDialog extends Dialog {
+
+    public SetAlarmDialog(@NonNull Context context) {
+        super(context);
+    }
+}
