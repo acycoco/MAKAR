a490b430ab9b7b7ed856d8a4c22bce7dddb67125
daeun084 <030804jk@naver.com>
2023-11-07 15:33:56 +0900

    refactor: NonRouteMainActivity 파일 제거, MainActivity 파일 병합

--- app/src/main/java/com/example/makar/MainActivity.java
+++ app/src/main/java/com/example/makar/MainActivity.java
@@ -3,15 +3,17 @@ package com.example.makar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 
 import com.example.makar.Dialog.SetAlarmDialog;
+import com.example.makar.Dialog.SetFavoriteStationDialog;
 import com.example.makar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean isRouteSet = false;
     SetAlarmDialog setAlarmDialog;
 
     @Override
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbarRouteMain);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
+
+        /**==경로 설정 Main==**/
 
         //시간표 버튼 클릭 리스너
         binding.timetableBtn.setOnClickListener(view ->{
@@ -39,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
         //경로 변경하기 버튼 클릭 리스너
         binding.changeRouteBtn.setOnClickListener(view -> {
         });
+
+
+        /**==경로 미설정 Main==**/
+
+        //경로 설정 버튼 클릭 리스너
+        binding.setRouteBtn.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, SetRouteActivity.class));
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(isRouteSet){
+            //route 설정된 메인화면
+            setFavoriteStation();
+        }
+        else {
+            //route 미설정 화면
+        }
     }
 
     @Override
@@ -46,5 +70,11 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater ().inflate (R.menu.menu_toolbar, menu);
 
         return true;
+    }
+
+    //자주 가는 역 설정 다이얼로그
+    private void setFavoriteStation(){
+        SetFavoriteStationDialog setFavoriteStationDialog = new SetFavoriteStationDialog(this);
+        setFavoriteStationDialog.show();
     }
 }
