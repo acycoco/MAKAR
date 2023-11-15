1da51c0c2be39e0c38034a622d513bf1e7af4142
Bibi-urssu <bibi.urssu@gmail.com>
2023-11-16 08:56:18 +0900

    feat: 역 검색

--- app/src/main/java/com/example/makar/SearchDepartureActivity.java
+++ app/src/main/java/com/example/makar/SearchDepartureActivity.java
@@ -1,18 +1,22 @@
 package com.example.makar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Context;
-import android.widget.SearchView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 
+import com.example.makar.Data.DataConverter;
 import com.example.makar.Data.Station;
 import com.example.makar.databinding.ActivitySearchDepartureBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,25 +27,28 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchDepartureActivity extends AppCompatActivity {
-    ActivitySearchDepartureBinding setFavoriteRouteBinding;
+    ActivitySearchDepartureBinding searchDepartureBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_departure);
 
-        setFavoriteRouteBinding = ActivitySearchDepartureBinding.inflate(getLayoutInflater());
-        setContentView(setFavoriteRouteBinding.getRoot());
+        searchDepartureBinding = ActivitySearchDepartureBinding.inflate(getLayoutInflater());
+        setContentView(searchDepartureBinding.getRoot());
 
-        setSupportActionBar(setFavoriteRouteBinding.toolbarSearchDeparture.getRoot());
+        setSupportActionBar(searchDepartureBinding.toolbarSearchDeparture.getRoot());
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        setFavoriteRouteBinding.toolbarSearchDeparture.toolbarText.setText("출발역 입력");
-        setFavoriteRouteBinding.toolbarSearchDeparture.toolbarImage.setVisibility(View.GONE);
-        setFavoriteRouteBinding.toolbarSearchDeparture.toolbarButton.setVisibility(View.GONE);
+        searchDepartureBinding.toolbarSearchDeparture.toolbarText.setText("출발역 입력");
+        searchDepartureBinding.toolbarSearchDeparture.toolbarImage.setVisibility(View.GONE);
+        searchDepartureBinding.toolbarSearchDeparture.toolbarButton.setVisibility(View.GONE);
 
         View rootView = findViewById(android.R.id.content);
 
@@ -54,8 +61,16 @@ public class SearchDepartureActivity extends AppCompatActivity {
             }
         });
 
+//        DataConverter databaseConverter = new DataConverter(this);
+//        databaseConverter.readExcelFileAndSave();
+
+        ListView listView = findViewById(R.id.search_departure_list_view);
+        List<Station> resultList = new ArrayList<>();
+        CustomAdapter adapter = new CustomAdapter(this, resultList);
+        listView.setAdapter(adapter);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        setFavoriteRouteBinding.searchViewDeparture.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchDepartureBinding.searchViewDeparture.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -76,27 +91,20 @@ public class SearchDepartureActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if (task.isSuccessful()) {
-                                StringBuilder result = new StringBuilder();
+                                resultList.clear();
                                 for (QueryDocumentSnapshot document : task.getResult()) {
                                     Station station = document.toObject(Station.class);
-
-                                    //TODO 받은 station(검색결과)을 화면에 어떻게 띄울지
-                                    result.append("역 이름: ").append(station.getStationName()).append("\n");
-                                    result.append("역 코드: ").append(station.getStationCode()).append("\n");
-                                    result.append("노선 이름: ").append(station.getLineNum()).append("\n");
-                                    result.append("철도 운영 기관 코드: ").append(station.getRailOpr()).append("\n\n");
+                                    resultList.add(station);
                                 }
-
-                                Log.d("MAKAR", "검색 결과:\n" + result.toString());
+                                adapter.notifyDataSetChanged();
                             } else {
                                 Log.d("MAKAR", "검색 중 오류 발생: ", task.getException());
                             }
                         }
-
                     });
+
                 }
                 return true;
-
             }
         });
     }
