f61296ab50c2a1dd9c796d5a1d0b146ddf7628b2
daeun084 <030804jk@naver.com>
2023-11-27 04:14:11 +0900

    feat: 최근경로 RecyclerView 구현

--- app/src/main/java/com/example/makar/data/Adapter/RouteAdapter.java
+++ app/src/main/java/com/example/makar/data/Adapter/RouteAdapter.java
@@ -1,7 +1,6 @@
-package com.example.makar.data;
+package com.example.makar.data.Adapter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +8,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.makar.data.BriefStation;
+import com.example.makar.data.Route;
 import com.example.makar.databinding.RouteRecyclerViewItemBinding;
 import com.example.makar.main.OnRouteClickListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> {
@@ -25,10 +25,10 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
         this.context = context;
         this.items = routeList;
     }
+
     public void setOnRouteClickListener(OnRouteClickListener listener) {
         this.listener = listener;
     }
-
 
 
     @NonNull
@@ -42,9 +42,15 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         route = items.get(position);
         List<BriefStation> briefStations = route.getBriefRoute();
+        String text = briefStations.get(0).getStationName();
 
-        holder.subRouteAdapter.setData(briefStations);
-        holder.subRouteAdapter.notifyDataSetChanged();
+        for(int i=1; i<briefStations.size(); i++){
+            String stationName = briefStations.get(i).getStationName();
+            text = text +" > "+stationName;
+        }
+
+        holder.binding.lineTextView.setText(text);
+
 
         holder.binding.lineTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,13 +69,11 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         RouteRecyclerViewItemBinding binding;
-        public SubRouteAdapter subRouteAdapter;
 
         public ViewHolder(RouteRecyclerViewItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
 
-            subRouteAdapter = new SubRouteAdapter(new ArrayList<>());
         }
 
     }
