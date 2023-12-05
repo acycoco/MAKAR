af38e48ff34449a88e846cb92b584ee80e0fbfce
daeun084 <030804jk@naver.com>
2023-12-05 14:22:41 +0900

    chore: 시간표 버튼 제거

--- app/src/main/java/com/example/makar/main/MainActivityChangeView.java
+++ app/src/main/java/com/example/makar/main/MainActivityChangeView.java
@@ -8,7 +8,7 @@ public class MainActivityChangeView {
     static void changeView(ActivityMainBinding mainBinding, boolean isRouteSet, long leftTime, String source, String destination) {
         if (isRouteSet) {
             //route 설정된 메인화면
-            mainBinding.timetableBtn.setVisibility(View.VISIBLE);
+            mainBinding.resetRouteBtn.setVisibility(View.VISIBLE);
             mainBinding.mainRouteView.setText(source + "  ->  " + destination); //출발지, 도착지
             mainBinding.mainTitleText.setText("막차까지 " + leftTime + "분 남았습니다"); //막차까지 남은 시간
             mainBinding.mainDestinationText.setText(source); //도착지 이름
@@ -19,7 +19,7 @@ public class MainActivityChangeView {
             mainBinding.recentRouteText.setVisibility(View.GONE);
         } else {
             //route 미설정 화면
-            mainBinding.timetableBtn.setVisibility(View.GONE);
+            mainBinding.resetRouteBtn.setVisibility(View.GONE);
             mainBinding.mainRouteView.setText("출발역  ->  도착역");
             mainBinding.mainTitleText.setText("경로를 설정해주세요");
             mainBinding.mainDestinationText.setText("MAKAR");
