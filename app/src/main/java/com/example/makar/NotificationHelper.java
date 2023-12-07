1b2a12c3912e91e8b0e6468c66aad4d42bf364fa
daeun084 <030804jk@naver.com>
2023-12-07 10:59:16 +0900

    fix: notification 구조 수정 : head-up

--- app/src/main/java/com/example/makar/NotificationHelper.java
+++ app/src/main/java/com/example/makar/NotificationHelper.java
@@ -1,10 +1,16 @@
 package com.example.makar;
+
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 import androidx.core.app.NotificationCompat;
+
+import com.example.makar.main.MainActivity;
 
 public class NotificationHelper {
     private static NotificationManager notificationManager;
@@ -17,7 +23,7 @@ public class NotificationHelper {
             String name = "MAKAR";
             String descriptionText = "MAKAR Nofitication";
 
-            NotificationChannel channel = new NotificationChannel("1", name, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel("1", name, NotificationManager.IMPORTANCE_HIGH);
             channel.setDescription(descriptionText);
             channel.enableVibration(true);
             channel.enableLights(true);
@@ -31,15 +37,24 @@ public class NotificationHelper {
             builder = new NotificationCompat.Builder(context);
         }
 
+        //set intent
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
         //show notification
-        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setSmallIcon(R.drawable.ic_notification_icon);
         builder.setWhen(System.currentTimeMillis());
         builder.setContentTitle(title);
         builder.setContentText(text);
         builder.setAutoCancel(true);
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setDefaults(Notification.DEFAULT_ALL);
+        builder.setFullScreenIntent(fullScreenPendingIntent, true);
 
-        notificationManager.notify(222, builder.build());
+        notificationManager.notify(1, builder.build());
 
-        Log.d("makar", "Show Notification");
+        Log.d("MAKAR", "Show Notification");
     }
 }
